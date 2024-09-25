79bb7729617d55b2af3bfe464be405570edf70a0
Thomas CASTELLY <thomas@castelly.fr>
2024-09-25 16:18:02 +0200

    release - export mapToDbDate

--- src/_base/dto/index.ts
+++ src/_base/dto/index.ts
@@ -1,11 +1,12 @@
 import boolean from './boolean';
-import dbDate from './dbDate';
+import dbDate, { mapToDbDate } from './dbDate';
 import dto from './dto';
 import hidden from './hidden';
 
 export {
   dto,
   dbDate,
+  mapToDbDate,
   boolean,
   hidden,
 };
