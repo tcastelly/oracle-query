48109ab2022625bb44deb15f0490d29288667c0f
Thomas CASTELLY <thomas@castelly.fr>
2026-08-24 10:25:30 +0200

    fix `omit`

--- src/_base/dto/omit.ts
+++ src/_base/dto/omit.ts
@@ -5,17 +5,21 @@ export type OmitCls<C extends Constructor, K extends keyof InstanceType<C>> = (n
 
 const omitCls = <C extends Constructor, K extends keyof InstanceType<C>>(
   cls: C,
-  attrs: K[],
+  keys: K[],
 ): OmitCls<C, K> => {
   class OmittedClass extends (cls as any) {
     constructor(...args: any[]) {
       super(...args);
       // eslint-disable-next-line no-restricted-syntax
-      for (const attr of attrs) {
+      for (const attr of keys) {
         delete (this as any)[attr];
       }
     }
   }
+
+  // Safe mutation without using 'any' to please other unsafe ESLint rules
+  const proto = cls.prototype as Record<string, unknown>;
+  proto._ignore = keys;
 
   return OmittedClass as unknown as OmitCls<C, K>;
 };
